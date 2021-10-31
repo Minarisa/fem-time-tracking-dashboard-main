f68bbe551f482bea39586fdd9a7aad17301d239e
Minarisa <87968210+Minarisa@users.noreply.github.com>
2021-10-31 20:42:55 +0100

    HTML, CSS ans JS updated

--- script.js
+++ script.js
@@ -1,40 +1,119 @@
-const WORK_CARD = document.getElementById("work");
-const WORK_CURRENT_TTLE = WORK_CARD.getElementsByClassName("current-stat__title")[0];
-const WORK_CURRENT_TIME = WORK_CARD.getElementsByClassName("current-stat__time")[0];
+const DAILY = document.getElementById("daily");
+const WEEKLY = document.getElementById("weekly");
+const MONTHLY = document.getElementById("monthly");
 
-const PLAY_CARD = document.getElementById("play");
-const PLAY_CURRENT_TTLE = PLAY_CARD.getElementsByClassName("current-stat__title")[0];
-const PLAY_CURRENT_TIME = PLAY_CARD.getElementsByClassName("current-stat__time")[0];
+const PREVIOUS_STATS = document.querySelectorAll(".card p:last-child");
+const CURRENT_STATS = document.querySelectorAll(".card p:nth-of-type(1)");
 
-const STUDY_CARD = document.getElementById("study");
-const STUDY_CURRENT_TTLE = STUDY_CARD.getElementsByClassName("current-stat__title")[0];
-const STUDY_CURRENT_TIME = STUDY_CARD.getElementsByClassName("current-stat__time")[0];
+let HTML_changer = (stats) => {
+    let hour_format;
+    let num;
 
-const EXERCISE_CARD = document.getElementById("exercise");
-const EXERCISE_CURRENT_TTLE = EXERCISE_CARD.getElementsByClassName("current-stat__title")[0];
-const EXERCISE_CURRENT_TIME = EXERCISE_CARD.getElementsByClassName("current-stat__time")[0];
+    DAILY.classList.remove("active");
+    WEEKLY.classList.remove("active");
+    MONTHLY.classList.remove("active");
 
-const SOCIAL_CARD = document.getElementById("social");
-const SOCIAL_CURRENT_TTLE = SOCIAL_CARD.getElementsByClassName("current-stat__title")[0];
-const SOCIAL_CURRENT_TIME = SOCIAL_CARD.getElementsByClassName("current-stat__time")[0];
+    if (stats == DAILY) {
+        DAILY.className = "active";
 
-const SELF_CARE_CARD = document.getElementById("self-care");
-const SELF_CARE_CURRENT_TTLE = SELF_CARE_CARD.getElementsByClassName("current-stat__title")[0];
-const SELF_CARE_CURRENT_TIME = SELF_CARE_CARD.getElementsByClassName("current-stat__time")[0];
+        for(i = 0; i < CURRENT_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.daily.current;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            CURRENT_STATS[i].innerHTML = `${num}${hour_format}`;
+        }
+
+        for(i = 0; i < PREVIOUS_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.daily.previous;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            PREVIOUS_STATS[i].innerHTML = `Previous day - ${num}${hour_format}`;
+        }
+
+    } else if (stats == WEEKLY) {
+        WEEKLY.className = "active";
+
+        for(i = 0; i < CURRENT_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.weekly.current;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            CURRENT_STATS[i].innerHTML = `${num}${hour_format}`;
+        }
+
+        for (i = 0; i < PREVIOUS_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.weekly.previous;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            PREVIOUS_STATS[i].innerHTML = `Last week - ${num}${hour_format}`;
+        }
+
+    } else if (stats == MONTHLY) {
+        MONTHLY.classList = "active";
+
+        for(i = 0; i < CURRENT_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.monthly.current;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            CURRENT_STATS[i].innerHTML = `${num}${hour_format}`;
+        }
+
+        for (i = 0; i < PREVIOUS_STATS.length; i++) {
+            num = DASHBOARD_STATS[i].timeframes.monthly.previous;
+            if (num == 1) {
+                hour_format = "hr";
+            } else {
+                hour_format = "hrs";
+            }
+            PREVIOUS_STATS[i].innerHTML = `Last month - ${num}${hour_format}`;
+        }
+
+    } else {
+        alert("ERROR OCCURRED, please reload the site.")
+    }
+}
+
+let button_handler = (param) => {
+    let stats;
+    if (param == "daily") {
+        stats = DAILY;
+    } else if (param == "weekly") {
+        stats = WEEKLY;
+    } else if (param == "monthly") {
+        stats = MONTHLY;
+    } else {
+        alert("ERROR OCCURRED, please reload the site.");
+        return;
+    }
+    HTML_changer(stats);
+}
 
 
+DAILY.addEventListener("click", () => {button_handler("daily")});
+WEEKLY.addEventListener("click", () => {button_handler("weekly")});
+MONTHLY.addEventListener("click", () => {button_handler("monthly")});
 
 
-
-
-
-
+let DASHBOARD_STATS;
 let requestUrl = "./data.json";
 let request = new XMLHttpRequest();
 request.open('GET', requestUrl);
 request.responseType = 'json';
 request.send();
 request.onload = () => {
-    const DASHBOARD_STATS = request.response;
-    console.log(DASHBOARD_STATS); /*test*/
+    DASHBOARD_STATS = request.response;
 }
